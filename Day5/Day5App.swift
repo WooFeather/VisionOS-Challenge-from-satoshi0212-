316eed7101efe4da0970a555c6360812ed52b3b5
woboneyo <bluewing1004@gmail.com>
2024-04-11 22:51:53 +0900

    Initial Commit

---
+++ Day5/Day5App.swift
@@ -0,0 +1,17 @@
+//
+//  Day5App.swift
+//  Day5
+//
+//  Created by 조우현 on 4/11/24.
+//
+
+import SwiftUI
+
+@main
+struct Day5App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
